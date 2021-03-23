8a6677f4c765f9b45bd09f742575901f41d57f9b
Jawad Adil <jawad.adil@student.griffith.ie>
2021-03-23 19:33:41 +0000

    added form validations + handled get requests for every page

--- main.py
+++ main.py
@@ -8,36 +8,45 @@ from google.auth.transport import requests
 import functions
 from holder_classes import GPU_info
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "jawad1.json"
 
 app = Flask(__name__)
 app.secret_key = 'BAD_SECRET_KEY'
 datastore_client = datastore.Client()
 firebase_request_adapter = requests.Request()
 
-@app.route('/add_gpu', methods=['POST'])
+@app.route('/add_gpu', methods=['POST','GET'])
 def add_gpu():
+    if request.method == 'GET':
+        return root()
     return render_template('add_gpu.html')
 
-@app.route('/add_gpu_to_datastore',methods = ['POST'])
+@app.route('/add_gpu_to_datastore',methods = ['POST','GET'])
 def add_gpu_to_datastore():
+    if request.method == 'GET':
+        return root()
     return functions.add_gpu_to_datastore()
 
-@app.route('/show_details',methods = ['POST'])
+@app.route('/show_details',methods = ['POST','GET'])
 def show_details_of_GPU():
+    if request.method == 'GET':
+        return root()
     gpu_name = request.form['gpu_name']
     gpu_data = functions.get_gpu_by_name(gpu_name)
     return render_template('GPU_Details.html',gpu_data = gpu_data)
 
-@app.route('/edit_gpu',methods = ['POST'])
+@app.route('/edit_gpu',methods = ['POST','GET'])
 def edit_gpu():
+    if request.method == 'GET':
+        return root()
     gpu_name = request.form['gpu_name']
     entity_key = datastore_client.key('GPUInfo', gpu_name)
     gpu_data = functions.get_gpu_by_name(gpu_name)
     return render_template('edit_gpu.html',gpu_data = gpu_data)
 
-@app.route('/update_details',methods = ['POST'])
+@app.route('/update_details',methods = ['POST','GET'])
 def update_details():
+    if request.method == 'GET':
+        return root()
     obj = GPU_info(request.form['name'],request.form['manufacturer'],request.form['issue_date'],request.form['geometryShader'],request.form['tesselationShader'],request.form['shaderInt16'],request.form['sparseBinding'],request.form['textureCompressionETC2'],request.form['vertexPipelineStoresAndAtomics'])
     old_name = request.form['old_name']
     obj1 = functions.get_gpu_by_name(old_name)
@@ -48,19 +57,25 @@ def update_details():
     else:
         return render_template('error.html',error="Record Already Exists")
 
-@app.route('/add_filters',methods=['POST'])
+@app.route('/add_filters',methods=['POST','GET'])
 def add_filters():
+    if request.method == 'GET':
+        return root()
     return functions.show_with_filters()
 
 
-@app.route('/compare',methods=['POST'])
+@app.route('/compare',methods=['POST','GET'])
 def compare():
+    if request.method == 'GET':
+        return root()
     GPU_list1 = functions.get_all_gpus()
     GPU_list2 = functions.get_all_gpus()
     return render_template('compare.html',GPU_list1 = GPU_list1,GPU_list2 = GPU_list2)
 
-@app.route('/comparison',methods=['POST'])
+@app.route('/comparison',methods=['POST','GET'])
 def comparison():
+    if request.method == 'GET':
+        return root()
     # user_info = functions.get_user_data()
     gpu1 = request.form['gpu1']
     gpu2 = request.form['gpu2']
